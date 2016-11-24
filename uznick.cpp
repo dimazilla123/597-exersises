30b3876c6bbf01aae9c58b4859cf32b8dd264525
dimazilla123 <evtushenko.2002@gmail.com>
2016-11-24 22:54:42 +0400

    Add files via upload

--- uznick.cpp
+++ uznick.cpp
@@ -2,7 +2,18 @@
 
 int main()
 {
+	int a,b,c,d,e;
+	bool yes[6]={false,false,false,false,false,false};
+	std::cin>>a>>b>>c>>d>>e;
+	yes[0]=!((a>d)&&(b>e));
+	yes[1]=!((b>d)&&(a>e));
+	yes[2]=!((a>d)&&(c>e));
+	yes[3]=!((c>d)&&(a>e));
+	yes[4]=!((b>d)&&(c>e));
+	yes[5]=!((c>d)&&(b>e));
 
-
+	bool answer=yes[0]||yes[1]||yes[2]||yes[3]||yes[4]||yes[5];
+	if(answer) std::cout<<"YES\n";
+	else std::cout<<"NO\n";
 	return 0;
 }
